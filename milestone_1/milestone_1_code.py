7ead99dee048c5392009ea849f0f05d97f287228
Dare666cjh <huangy@wsc.school.nz>
2025-05-19 11:58:44 +1200

    milestone1

--- milestone_1/milestone_1_code.py
+++ milestone_1/milestone_1_code.py
@@ -1,1 +1,15 @@
+# Are you eat?
+# by Dare
 
+print("Hello! I'm your 'Have You Eaten?' analyzer.")
+print("I'm new to coding, please be kind!")
+
+name = input("What's your name? ")
+eat = input("Have you eaten today? (Answer: yes/no) ")
+
+if eat == "yes":
+    print("Good job, " + name + "! You're doing great!")
+else:
+    print("Go eat something, or you'll be hungry!")
+
+print("Goodbye, " + name + "! Come chat with me again tomorrow!")
